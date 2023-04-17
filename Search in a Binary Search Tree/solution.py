cfa3774916e7e897921a1bb2639b60df2a5026dd
shivam <shivam@tensoriot.com>
2023-04-18 01:46:09 +0530

    added soultion and readme files

---
+++ Search in a Binary Search Tree/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        if val==root.val:
+            return root #[root.val,root.left.val,root.right.val]
+        elif val<root.val:
+            return self.searchBST(root.left,val)
+        elif val>root.val:
+            return self.searchBST(root.right,val)
+        else:
+            return []
