9a0a0124b39b77cc634323f670614664528844ee
shivam <shivam@tensoriot.com>
2023-05-12 19:08:37 +0530

    added solution and readme files

--- Permutations/solution.py
+++ Permutations/solution.py
@@ -1,5 +1,24 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         from itertools import permutations 
+#         return permutations(nums)
+        
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations 
-        return permutations(nums)
-        
+        ans=[]
+        freq=[0]*len(nums)
+        ds=[]
+
+        def solve(nums,ds,ans,freq):
+            if len(ds)==len(nums):
+                ans.append(ds.copy())
+                return
+            for i in range(len(nums)):
+                if freq[i]==0:
+                    freq[i]=1
+                    ds.append(nums[i])
+                    solve(nums,ds,ans,freq)
+                    freq[i]=0
+                    ds.pop(-1)
+        solve(nums,ds,ans,freq)
+        return ans
