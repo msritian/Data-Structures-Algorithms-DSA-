54357b9096b94e61272858b06d8f85b1cf383a88
shivam <shivam@tensoriot.com>
2023-05-18 03:03:16 +0530

    added solution and readme files

--- Fruit Into Baskets/solution.py
+++ Fruit Into Baskets/solution.py
@@ -1,8 +1,15 @@
+from collections import defaultdict
+from collections import Counter
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        mp={i:0 for i in fruits}
+        # mp={i:0 for i in fruits}
+        mp = defaultdict(int)
         i,j,mx=0,0,0
         k=2
+        if len(fruits)<=2:
+            return len(fruits)
+        if len(Counter(fruits))<2:
+            return len(fruits)
         while j<len(fruits):
             mp[fruits[j]]+=1
 
@@ -12,10 +19,11 @@ class Solution:
                 mx=max(mx,j-i+1)
                 j+=1
             elif len(mp)>k:
+                print(mp)
                 while len(mp)>k:
                     mp[fruits[i]]-=1
                     if mp[fruits[i]]==0:
-                        mp.pop(fruits[i])
+                        del mp[fruits[i]]
                     i+=1
                 j+=1
         return mx
