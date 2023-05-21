42812cbadccc7ed2e4081c548f6e7fd305391b2f
shivam <shivam@tensoriot.com>
2023-05-21 17:36:22 +0530

    added solution and readme files

---
+++ Longest Palindromic Subsequence/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        #Using DP
+        m=len(s)
+        s2=s[::-1]
+        n=len(s2)
+        arr=[["" for j in range(n+1)] for i in range(m+1)]
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0 or j==0:
+                    arr[i][j]=0
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if s[i-1]==s2[j-1]:
+                    arr[i][j]= 1+arr[i-1][j-1]
+                else:
+                    arr[i][j]=max(arr[i-1][j],arr[i][j-1])
+        return arr[m][n]
