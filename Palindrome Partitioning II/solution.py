8c2e3b2e917cd29570459ff660eb2452a203c142
shivam <shivam@tensoriot.com>
2023-05-30 00:25:03 +0530

    added solution and readme files

--- Palindrome Partitioning II/solution.py
+++ Palindrome Partitioning II/solution.py
@@ -7,21 +7,24 @@ class Solution:
         def solve(s,i,j):
             if i>=j:
                 return 0
+            if t[i][j]!=-1:
+                return t[i][j]
             if s[i:j+1]==s[i:j+1][::-1]:
                 t[i][j]=0
                 return 0
-            if t[i][j]!=-1:
-                return t[i][j]
+            
             mn=float('inf')
             for k in range(i,j):
                 if t[i][k]!=-1:
                     left=t[i][k]
                 else:
                     left=solve(s,i,k)
+                    t[i][k]=left
                 if t[k+1][j]!=-1:
                     right=t[k+1][j]
                 else:
                     right=solve(s,k+1,j)
+                    t[k+1][j]=right
                 temp=1+left+right
                 if temp<mn:
                     mn=temp
