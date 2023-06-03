82d53f466144c5e6f7a319665f99c3a62a626cf4
shivam <shivam@tensoriot.com>
2023-06-04 00:52:38 +0530

    added solution and readme files

---
+++ Coin Change II/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        add=0
+        target=amount
+        nums=coins
+        for i in nums:
+            add+=i
+        t=[["" for j in range(target+1)] for i in range(len(nums)+1)]
+        for i in range(len(nums)+1):
+            for j in range(target+1):
+                if i==0:
+                    t[i][j]=0
+                if j==0:
+                    t[i][j]=1
+        for i in range(1,len(nums)+1):
+            for j in range(target+1):
+                if nums[i-1]<=j:
+                    t[i][j]=t[i][j-nums[i-1]]+t[i-1][j]
+                else:
+                    t[i][j]=t[i-1][j]
+        return t[len(nums)][target]
